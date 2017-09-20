2cd70112dcf3e7b422e318b3db2c07fa84ebe0cb
SSW-SCIENTIFIC <S-sword@s-sword.net>
2017-09-20 17:23:13 +0900

    Ranking追加

---
+++ lib/Ranking/CategoryList.d.ts
@@ -0,0 +1,18 @@
+export declare enum Period {
+    HOURLY = "hourly",
+    DAILY = "daily",
+    WEEKLY = "weekly",
+    MONTHLY = "monthly",
+    ALL = "all",
+}
+export declare enum Type {
+    TOTAL = "fav",
+    VIEW = "view",
+    COMMENT = "res",
+    MYLIST = "mylist",
+}
+export interface Category {
+    title: string;
+    suffix: string;
+    children?: Array<Category>;
+}
